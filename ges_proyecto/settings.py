842c9462460b908cd92bd1edc75d191e2eff5d9d
FabianRom <fabianrromero@yahoo.com.ar>
2025-11-11 21:08:40 -0300

    Ajustes de settings para Railway

--- ges_proyecto/settings.py
+++ ges_proyecto/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url # type: ignore
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,12 +26,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w1-@ovl@-1yhfh1f$m-fn!5-baa^s@1(h&j6rxd!m7zkukiaap'
+SECRET_KEY = os.getenv("SECRET_KEY", "insegura")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
@@ -81,15 +85,9 @@ WSGI_APPLICATION = 'ges_proyecto.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ges_proyecto_db',  # Reemplaza con el nombre de tu base de datos
-        'USER': 'root', # Reemplaza con tu usuario de MySQL
-        'PASSWORD': 'Espana2025@', # Reemplaza con tu contraseña de MySQL
-        'HOST': 'localhost', # Generalmente es 'localhost'
-        'PORT': '3306', # Puerto estándar de MySQL
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
